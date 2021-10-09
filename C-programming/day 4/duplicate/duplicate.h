6c7c2d6b625be8ac2f5f19884e4260fbf49ff89a
deepthikm27 <deepthikm.27@gmail.com>
2021-10-09 13:48:39 +0530

    day2

--- C-programming/day 4/duplicate/duplicate.h
+++
@@ -1,7 +0,0 @@
-//Remove the duplicate entries in the array
-int remove_duplicate_elements(int [], int);
-
-// search a given integer in an array using the linear search technique
-int linear_search_array(int arr[], int size, int search_element);
-
-
